213bc95749f20f6f9dd0e4e56042c7729bc996e5
krystian137 <krychac8@gmail.com>
2025-10-21 11:50:31 +0200

    removed contact

--- routes.py
+++ routes.py
@@ -1,16 +1,13 @@
-import os
-import smtplib
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 
 main = Blueprint('main', __name__)
 
-EMAIL_KEY = ""
-PASSWORD_KEY = ""
 
 @main.route('/set_language/<lang_code>')
 def set_language(lang_code):
     session['lang'] = lang_code
     return redirect(request.referrer or url_for('main.index'))
+
 
 @main.route('/')
 def index():
@@ -20,6 +17,7 @@ def index():
 @main.route('/moviereviewers')
 def movie_reviewers():
     return render_template("moviereviewers.html")
+
 
 @main.route('/myfilmsay')
 def myfilmsay():
@@ -34,23 +32,3 @@ def steam_chests():
 @main.route('/otherprojects')
 def other_projects():
     return render_template("otherprojects.html")
-
-
-MAIL_ADDRESS = os.environ.get("EMAIL_KEY")
-MAIL_main_PW = os.environ.get("PASSWORD_KEY")
-
-@main.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        data = request.form
-        send_email(data["name"], data["email"], data["message"])
-        return render_template("contact.html", msg_sent=True)
-    return render_template("contact.html", msg_sent=False)
-
-
-def send_email(name, email, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-        connection.starttls()
-        connection.login(MAIL_ADDRESS, MAIL_main_PW)
-        connection.sendmail(MAIL_ADDRESS, MAIL_ADDRESS, email_message)
